f99853e8d846bfc01da063e908feb8dbd96b97f5
Java Developer Pro. Rust & Clojure enthusiast <151893782+pmarat-dev@users.noreply.github.com>
2024-05-01 17:53:25 +0200

    Macro for registry backbone. (#8)
    Macro for registry backbone.
    New inmemory_async implementation.

--- src/inmemory_async.rs
+++ src/inmemory_async.rs
@@ -4,12 +4,12 @@ create_registry_backbone!();
 
 impl EventHandlerRegistry for EventHandlerRegistryImpl {
     fn register(&mut self, event_handler: Box<dyn EventHandler + Send>) {
-        println!("Sync in-memory event handler registered: {}", event_handler);
+        println!("Async in-memory event handler registered: {}", event_handler);
         self.handlers.push(event_handler);
     }
 
     fn emit(&self, event: &dyn Event) {
-        println!("Sync event emitted: {}", event);
+        println!("Async event emitted: {}", event);
         for handler in self.handlers.iter() {
             handler.handle(event);
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 pub mod model;
 pub mod inmemory_sync;
+pub mod inmemory_async;
+pub mod macros;
