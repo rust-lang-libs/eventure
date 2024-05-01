2ef14a778bd8ddef286a32c9ebb75dd36541bfcb
Piotr <pmarat.dev@gmail.com>
2024-05-01 21:17:03 +0200

    Created shared module for examples.

--- examples/src/inmemory_async/main.rs
+++ examples/src/inmemory_async/main.rs
@@ -1,11 +1,13 @@
+use eventure_examples::shared::order_created;
+use eventure_examples::shared::order_canceled;
 use eventure::inmemory_async;
-use eventure_examples::{order_canceled, order_created};
 
 fn main() {
     let order_created = order_created::create();
     let order_canceled = order_canceled::create();
+    let order_created_handler = order_created::handler();
 
-    inmemory_async::register(order_created::handler());
+    inmemory_async::register(order_created_handler);
     inmemory_async::emit(&order_created);
     inmemory_async::emit(&order_canceled);
 }
--- examples/src/inmemory_sync/main.rs
+++ examples/src/inmemory_sync/main.rs
@@ -1,11 +1,13 @@
+use eventure_examples::shared::order_created;
+use eventure_examples::shared::order_canceled;
 use eventure::inmemory_sync;
-use eventure_examples::{order_canceled, order_created};
 
 fn main() {
     let order_created = order_created::create();
     let order_canceled = order_canceled::create();
+    let order_created_handler = order_created::handler();
 
-    inmemory_sync::register(order_created::handler());
+    inmemory_sync::register(order_created_handler);
     inmemory_sync::emit(&order_created);
     inmemory_sync::emit(&order_canceled);
 }
