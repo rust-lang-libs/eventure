98f0ba13c39a9c4863dc88efb90e4ed305e92173
Java Developer Pro. Rust enthusiast <151893782+pmarat-dev@users.noreply.github.com>
2024-05-04 14:13:03 +0200

    Added getting-started example. (#44)

---
+++ examples/src/getting-started/main.rs
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Rust-Lang Libs/Eventure 2024
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+
+use eventure::in_memory;
+use eventure::in_memory::ChannelType::TOPIC;
+use examples::shared::order_created;
+
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Main function
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+fn main() {
+    println!();
+    init_logger();
+
+    let order_created = order_created::create();
+    let configuration = in_memory::configuration(TOPIC, ".*", false);
+    in_memory::setup(configuration);
+
+    let handler_topic_channel = in_memory::message_channel(TOPIC, "Order");
+    let order_created_handler = order_created::handler();
+    in_memory::register(handler_topic_channel, order_created_handler);
+
+    in_memory::emit(&order_created);
+
+    println!();
+}
+
+fn init_logger() {
+    SimpleLogger::new()
+        .with_colors(false)
+        .with_level(LevelFilter::Debug)
+        .init().unwrap();
+}
