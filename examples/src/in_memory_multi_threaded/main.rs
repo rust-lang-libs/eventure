a8f44fbe1d757539e5b36ac54cb234da3280df57
P.Marat <151893782+pmarat-dev@users.noreply.github.com>
2024-05-05 17:45:35 +0200

    Coloring log targets. (#54)

--- examples/src/in_memory_multi_threaded/main.rs
+++ examples/src/in_memory_multi_threaded/main.rs
@@ -8,6 +8,7 @@ use simple_logger::SimpleLogger;
 
 use eventure::in_memory;
 use eventure::in_memory::ChannelType::TOPIC;
+
 use examples::shared::order_created;
 
 // -----------------------------------------------------------------------------------------------------------------------------------------
