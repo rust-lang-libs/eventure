e78581414ce838fd9ebd9df8a92d65c6b17e5703
Piotr <pmarat.dev@gmail.com>
2024-05-07 19:25:39 +0200

    First rough implementation of event handler in Kafka runtime.

--- examples/src/kafka/main.rs
+++ examples/src/kafka/main.rs
@@ -2,6 +2,7 @@
 // Rust-Lang Libs/Eventure 2024
 // -----------------------------------------------------------------------------------------------------------------------------------------
 
+use std::{thread, time};
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 use eventure::kafka;
@@ -20,6 +21,25 @@ fn main() {
 
     let order_created = order_created::create();
     kafka::emit(&order_created);
+
+    let message_channel = kafka::message_channel("orders", 0);
+    let order_created_handler = order_created::handler();
+    kafka::register(message_channel, order_created_handler);
+
+    let duration = time::Duration::from_secs(5);
+    thread::sleep(duration);
+
+    let order_created = order_created::create();
+    kafka::emit(&order_created);
+
+    let duration = time::Duration::from_secs(5);
+    thread::sleep(duration);
+
+    let order_created = order_created::create();
+    kafka::emit(&order_created);
+
+    let duration = time::Duration::from_secs(5);
+    thread::sleep(duration);
 
     println!();
 }
