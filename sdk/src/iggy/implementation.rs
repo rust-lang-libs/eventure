ed7e965bd9f2a270173421ca5503f23cf6d2c2e6
P.Marat <151893782+pmarat-dev@users.noreply.github.com>
2024-05-10 22:54:42 +0200

    Updated doc comments. (#72)

--- sdk/src/iggy/implementation.rs
+++ sdk/src/iggy/implementation.rs
@@ -6,17 +6,24 @@
 // Public structs
 // -----------------------------------------------------------------------------------------------------------------------------------------
 
+/// Iggy message channel definition.
 pub struct MessageChannel {
     pub stream_id: u32,
     pub topic_id: u32,
     pub partition_id: u32,
 }
 
+/// Iggy message broker configuration.
 pub struct MessageBrokerConfiguration {
     pub message_channel: MessageChannel,
     pub server: &'static str,
 }
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// Public functions
+// -----------------------------------------------------------------------------------------------------------------------------------------
+
+/// Creates Iggy message channel.
 pub fn message_channel(stream_id: u32, topic_id: u32, partition_id: u32) -> MessageChannel {
     MessageChannel {
         stream_id,
@@ -25,6 +32,7 @@ pub fn message_channel(stream_id: u32, topic_id: u32, partition_id: u32) -> Mess
     }
 }
 
+/// Creates Iggy message broker configuration.
 pub fn configuration(server: &'static str, message_channel: MessageChannel) -> MessageBrokerConfiguration {
     MessageBrokerConfiguration {
         message_channel,
